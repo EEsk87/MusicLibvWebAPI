aeb25d387600745f9873de2dc2954d4761235f93
Earl Eskridge <EJay@Cindys-Air.lan>
2023-10-30 19:38:12 -0400

    Added models and controllers also linked DB

---
+++ MusicLabWebAPI/Data/ApplicatioDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MusicLabWebAPI.Models;
+
+namespace MusicLabWebAPI.Data
+{
+	public class ApplicationDbContext : DbContext
+	{ 
+	
+        public DbSet<Song> Songs { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+			
+		{
+
+		}
+	}
+}
+
---
+++ MusicLabWebAPI/Models/Song.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicLabWebAPI.Models
+{
+
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        
+           
+
+        
+
+
+
+    }
+
+}
+
+
+
+
+
+   
+
+
+          
+
+
+     
+
